646e28766dcc18a9db4bd9aca0d0cac6a7d8817d
Loshiga Mohan <loshiga.mohan@student.griffith.ie>
2021-04-16 21:07:58 +0100

    removed comments

--- main.py
+++ main.py
@@ -9,32 +9,6 @@ datastore_client = datastore.Client()
 firebase_request_adapter = requests.Request()
 
 
-#name_key = datastore_client.key("Name", "Name1")
-#name_task = datastore.Entity(key=name_key)
-#datastore_client.put(name_task)
-
-#manufacturer_key = datastore_client.key("Manufacturer", "Manufacturer1")
-#manufacturer_task = datastore.Entity(key=manufacturer_key)
-#datastore_client.put(manufacturer_task)
-
-#geometryShader_entity = datastore.Entity(key = datastore_client.key('geometryShader'))
-#datastore_client.put(geometryShader_entity)
-
-#tesselationShader_entity = datastore.Entity(key = datastore_client.key('tesselationShader'))
-#datastore_client.put(tesselationShader_entity)
-
-#shaderInt16_entity = datastore.Entity(key = datastore_client.key('shaderInt16'))
-#datastore_client.put(shaderInt16_entity)
-
-#sparseBinding_entity = datastore.Entity(key = datastore_client.key('sparseBinding'))
-#datastore_client.put(sparseBinding_entity)
-
-#textureCompressionETC2_entity = datastore.Entity(key = datastore_client.key('textureCompressionETC2'))
-#datastore_client.put(textureCompressionETC2_entity)
-
-#vertexPipelineStoresAndAtomics_entity = datastore.Entity(key = datastore_client.key('vertexPipelineStoresAndAtomics'))
-#datastore_client.put(vertexPipelineStoresAndAtomics_entity)
-
 def getClientInfo():
     id_token = request.cookies.get("token")
     error_message = None
